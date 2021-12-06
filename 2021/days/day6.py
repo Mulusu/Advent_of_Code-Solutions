4955a3e8b42b6427bb1722e8510c918432a43335
Ville Sinkkonen <ville.p.sinkkonen@gmail.com>
2021-12-06 12:19:42 +0200

    cleanup of comments on day6

--- 2021/days/day6.py
+++ 2021/days/day6.py
@@ -1,9 +1,11 @@
+"""
+There are only 9 (0-8) possible states for the lanternfish
+Instead of simulating each fish individually, we just track how MANY fish is in each state,
+and shuffle and increment those numbers accordingly.
+"""
 
 def main(inputstr):
     timers = list(map(int, inputstr.split(","))) # Split input and convert to ints
-    # There are only 9 (0-8) possible states for the lanternfish
-    # Instead of simulating each, we just track how MANY of each state we have
-    # and shuffle and increment those numbers accordingly.
     states = [0] * 9            # Allocate zeros for all states
     for i in range(9):
         states[i] = timers.count(i)
@@ -14,9 +16,9 @@ def main(inputstr):
 
 def simulate_lanternfish(days : int, states : list):
     for i in range(days):
-        atzero = states.pop(0)
-        states[6] += atzero # The old ones reset their timer to 6
-        states.append(atzero) # Spawn new ones at the last state
+        atzero = states.pop(0)  # The first state, representing fish with timer at zero
+        states[6] += atzero     # The old ones reset their timer to 6
+        states.append(atzero)   # The just spawned get their timer to 8 (last state)
     return sum(states)
 
 def part1(states):
