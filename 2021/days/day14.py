788ae5335e0bd1b8124a0331c89dcac23fb87217
Ville Sinkkonen <ville.p.sinkkonen@gmail.com>
2021-12-14 13:04:54 +0200

    2021 day 14

--- 2021/days/day14.py
+++ 2021/days/day14.py
@@ -1,45 +1,55 @@
-import re
-
 def main(inputstr):
     template, rulesstr = inputstr.split("\n\n")
     rules = {}
     for i in rulesstr.split("\n"):
         pair,fill = i.split(" -> ")
-        rules[pair] = fill
+        rules[pair] = [pair[0]+fill,fill+pair[1]]   # The new pairs that will be formed by this rule
     ans1 = part1(template, rules)
     ans2 = part2(template, rules)
     return ans1, ans2
 
-def pair_insert(template,rules,steps):
+
+def pair_insert(pairs,rules,steps):
     for _ in range(steps):
-        new_tmplt = ""
-        for i in range(len(template)-1):
-            pair = template[i]+template[i+1]
-            if pair in rules.keys():
-                new_tmplt += pair[0]
-                new_tmplt += rules[pair]
-            else:
-                new_tmplt += pair[0]
-        new_tmplt += template[-1]
-        template = new_tmplt
-    return template
+        newpairs = {}
+        for key in pairs.keys():                    # For every pair in the old string...
+            for insert in rules[key]:               # For every new pair that is formed by the rule it matches
+                if insert in newpairs.keys():       # Count the number that new pair will be in the new string
+                    newpairs[insert] += pairs[key]
+                else:
+                    newpairs[insert] = pairs[key]
+        pairs = newpairs
+    return pairs
+
 
 def part1(template, rules, steps = 10):
-    template = pair_insert(template,rules,steps)
+    # Build the pairs from the template
+    pairs = {}
+    for i in range(len(template)-1):
+        newpair = template[i]+template[i+1]
+        if newpair in pairs.keys():
+            pairs[newpair] += 1
+        else:
+            pairs[newpair] = 1
+
+    # Insert new elements according to rules
+    pairs = pair_insert(pairs,rules,steps)
     
     # Find max and min
     times = {}
-    for i in range(len(template)):
-        char = template[i]
+    for key in pairs.keys():
+        char = key[0]
         if char in times.keys():
-            times[char] += 1
+            times[char] += pairs[key]
         else:
-            times[char] = 1
+            times[char] = pairs[key]
+    times[template[-1]] += 1 # Last character of template is not counted yet
     vals = list(times.values())
     vals.sort()
     res = vals[-1] - vals[0]
     return res
 
+
 def part2(template, rules):
-#    res = part1(template,rules,40)
-    return "Not yet done"
+    res = part1(template,rules,40)  # Part 1 has everything, just call it
+    return res
